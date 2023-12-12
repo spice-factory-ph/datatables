69f5b24e4a2f443bc47932301bce525f402408ac
Ian Jay Bronola <ian.bronola@appetiser.com.au>
2023-12-13 07:27:21 +0800

    Refactor DataTableMakeCommand to use Artisan command

--- src/Providers/Console/Commands/DataTableMakeCommand.php
+++ src/Providers/Console/Commands/DataTableMakeCommand.php
@@ -4,35 +4,55 @@ declare(strict_types=1);
 
 namespace Ianjaybronola\SpiceDatatable\Providers\Console\Commands;
 
+use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Console\GeneratorCommand;
 use Symfony\Component\Console\Input\InputArgument;
 
-class DataTableMakeCommand extends GeneratorCommand
+class DataTableMakeCommand extends Command
 {
-    protected $name = 'make:spice-datatable {model}';
+    protected $signature = 'make:spice-datatable {model}';
 
     protected $description = 'Create a new datatable class';
 
-    protected function getStub()
-    {
-        return __DIR__ . "/../../../../stubs/scripts.stub";
-    }
+    // protected function getStub()
+    // {
+    //     return __DIR__ . "/../../../../stubs/scripts.stub";
+    // }
 
-    protected function getDefaultNamespace($rootNamespace): string
-    {
-        return $rootNamespace . '\SpiceDatatable\DataTables';
-    }
+    // protected function getDefaultNamespace($rootNamespace): string
+    // {
+    //     return $rootNamespace . '\DataTables';
+    // }
 
     /**
-     * Get the console command arguments.
+     * Execute the console command.
      *
-     * @return array
+     * @return void
      */
-    protected function getArguments()
+    public function handle()
     {
-        return [
-            ['name', InputArgument::REQUIRED, 'The name of the contract.'],
-        ];
+        $model = $this->argument('model');
+        // Execute the command 'sail artisan datatables:make {model}'
+        Artisan::call('datatables:make ' . $model);
+
+        // Log the created files
+        $this->output->writeln('Creating datatable for ' . $model . '...');
+        $this->output->writeln(Artisan::output());
     }
+
+
+
+    // /**
+    //  * Get the console command arguments.
+    //  *
+    //  * @return array
+    //  */
+    // protected function getArguments()
+    // {
+    //     return [
+    //         ['model', InputArgument::REQUIRED, 'The name of the model.'],
+    //     ];
+    // }
 }
