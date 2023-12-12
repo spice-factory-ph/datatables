a57d8e16935dd15b59f74d6f9c7eb6a6bd7ffa5d
Ian Jay Bronola <ian.bronola@appetiser.com.au>
2023-12-13 07:39:11 +0800

    Add ScriptsMakeCommand to DatatableServiceProvider

--- src/Providers/DatatableServiceProvider.php
+++ src/Providers/DatatableServiceProvider.php
@@ -6,6 +6,7 @@ namespace Ianjaybronola\SpiceDatatable\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Ianjaybronola\SpiceDatatable\Providers\Console\Commands\DataTableMakeCommand;
+use Ianjaybronola\SpiceDatatable\Providers\Console\Commands\ScriptsMakeCommand;
 
 final class DatatableServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,7 @@ final class DatatableServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->commands(
                 commands: [
+                    ScriptsMakeCommand::class,
                     DataTableMakeCommand::class,
                 ]
             );
