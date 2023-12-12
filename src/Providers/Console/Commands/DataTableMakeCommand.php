cedfbddd6ae1cfe7b1e70c2f53ddc77075b8f1f9
Ian Jay Bronola <ian.bronola@appetiser.com.au>
2023-12-12 16:19:04 +0800

    Refactor DataTableMakeCommand signature and stub path

--- src/Providers/Console/Commands/DataTableMakeCommand.php
+++ src/Providers/Console/Commands/DataTableMakeCommand.php
@@ -2,15 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Ianjaybronola\SpiceDatatable\Providers\Console\Commands;
+namespace Ianjaybronola\SpiceDatatable\Console\Commands;
 
+use Illuminate\Console\Command;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\GeneratorCommand;
 
-final class DataTableMakeCommand extends GeneratorCommand
+final class DataTableMakeCommand extends Command
 {
-    protected $signature = 'make:spice-datatable {model}';
+    protected $signature = 'make:spice-datatable {name: model name}';
 
     protected $description = 'Create a new datatable class';
 
@@ -25,22 +24,11 @@ final class DataTableMakeCommand extends GeneratorCommand
 
         $file = $readonly ? 'dto-82.stub' : 'dto.stub';
 
-        return __DIR__ . '/../../../../stubs/' . $file;
+        return __DIR__ . "/../../../stubs/{$file}";
     }
 
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace . '\DataTables';
-    }
-
-    public function handle()
-    {
-        $model = $this->argument('model');
-        // Execute the command 'sail artisan datatables:make {model}'
-        Artisan::call('datatables:make ' . $model);
-
-        // Log the created files
-        $this->output->writeln('Creating datatable for ' . $model . '...');
-        $this->output->writeln(Artisan::output());
     }
 }
