6d2ade0742d4af0ece3a7be951470ccfb8928e1e
Ian Jay Bronola <ian.bronola@appetiser.com.au>
2023-12-12 16:12:11 +0800

    Refactor DataTableMakeCommand signature and handle method

--- src/Providers/Console/Commands/DataTableMakeCommand.php
+++ src/Providers/Console/Commands/DataTableMakeCommand.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Ianjaybronola\SpiceDatatable\Providers\Console\Commands;
 
-use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\GeneratorCommand;
 
 final class DataTableMakeCommand extends GeneratorCommand
 {
-    protected $signature = 'make:spice-datatable';
+    protected $signature = 'make:spice-datatable {model}';
 
     protected $description = 'Create a new datatable class';
 
@@ -30,5 +31,16 @@ final class DataTableMakeCommand extends GeneratorCommand
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace . '\DataTables';
+    }
+
+    public function handle()
+    {
+        $model = $this->argument('model');
+        // Execute the command 'sail artisan datatables:make {model}'
+        Artisan::call('datatables:make ' . $model);
+
+        // Log the created files
+        $this->output->writeln('Creating datatable for ' . $model . '...');
+        $this->output->writeln(Artisan::output());
     }
 }
