3779ac700e65b795ba493bee13665d71c1a834dd
Ian Jay Bronola <ian.bronola@appetiser.com.au>
2023-12-13 19:24:56 +0800

    Refactor ScriptsMakeCommand to use GeneratorCommand

--- src/Providers/Console/Commands/ScriptsMakeCommand.php
+++ src/Providers/Console/Commands/ScriptsMakeCommand.php
@@ -5,25 +5,42 @@ declare(strict_types=1);
 namespace Ianjaybronola\SpiceDatatable\Providers\Console\Commands;
 
 use Illuminate\View\Factory;
-use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
 use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputArgument;
 
-class ScriptsMakeCommand extends Command
+class ScriptsMakeCommand extends GeneratorCommand
 {
-    protected $signature = "spice-make:scripts {name}";
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'spice-make:scripts';
+
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'Create scripts for datatable';
 
 
     /**
-     * @var File
+     * The type of class being generated.
+     *
+     * @var string
      */
-    private $file;
+    protected $type = 'Repository';
 
     /**
-     * @var Factory
+     * Execute the console command.
      */
-    private $view;
+    public function handle(): bool|null
+    {
+        return parent::handle();
+    }
 
     protected function getStub()
     {
@@ -31,39 +48,74 @@ class ScriptsMakeCommand extends Command
     }
 
     /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $modelName = strtolower($this->argument('name'));
+        $class = parent::buildClass($name);
+        $class = str_replace(['{{ model }}'], $modelName, $class);
+        $class = str_replace(['{{ model_variable }}'], strtolower($modelName), $class);
+
+        return $class;
+    }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+
+    /**
      * Create a new command instance.
      *
      * @param Filesystem $file
      * @param Factory    $viewFactory
      */
-    public function __construct(Filesystem $file, Factory $viewFactory)
-    {
-        $this->file = $file;
-        $this->view = $viewFactory;
+    // public function __construct(Filesystem $file, Factory $viewFactory)
+    // {
+    //     $this->file = $file;
+    //     $this->view = $viewFactory;
 
-        parent::__construct();
-    }
+    //     parent::__construct();
+    // }
 
     /**
      * Execute the console command.
      *
      * @return mixed
      */
-    public function handle()
+    // public function handle()
+    // {
+    //     $view = strtolower($this->argument('name'));
+    //     $path = 'resources/views/datatables/scripts/' . $view . '/scripts.blade.php';
+    //     $directory = dirname($path);
+
+    //     if ($this->file->exists($path)) {
+    //         $this->error("A view already exists at {$path}!");
+    //     }
+
+    //     if (!$this->file->exists($directory)) {
+    //         $this->file->makeDirectory($directory, 0777, true);
+    //     }
+
+    //     $this->file->put($path, $view);
+    //     $this->info("Created a new view at {$path}");
+    // }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
     {
         $view = strtolower($this->argument('name'));
-        $path = 'resources/views/datatables/scripts/' . $view . '/scripts.blade.php';
-        $directory = dirname($path);
-
-        if ($this->file->exists($path)) {
-            $this->error("A view already exists at {$path}!");
-        }
-
-        if (!$this->file->exists($directory)) {
-            $this->file->makeDirectory($directory, 0777, true);
-        }
-
-        $this->file->put($path, $view);
-        $this->info("Created a new view at {$path}");
+        return 'resources/views/datatables/scripts/' . $view . '/scripts.blade.php';
     }
 }
