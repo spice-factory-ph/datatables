01867d772ce91037380bb53a892bdf2c06f2a6f4
Ian Jay Bronola <ian.bronola@appetiser.com.au>
2023-12-13 08:58:15 +0800

    Update file path in ScriptsMakeCommand.php

--- src/Providers/Console/Commands/ScriptsMakeCommand.php
+++ src/Providers/Console/Commands/ScriptsMakeCommand.php
@@ -22,7 +22,7 @@ class ScriptsMakeCommand extends GeneratorCommand
     protected function getDefaultNamespace($rootNamespace): string
     {
         $scriptName = strtolower($this->argument('name'));
-        return 'resources\views\\' . $scriptName . '\\' . $scriptName . '\\scripts.js';
+        return config('view.paths') . '\\' . $scriptName . '\\' . $scriptName . '\\scripts.blade.php';
     }
 
     /**
