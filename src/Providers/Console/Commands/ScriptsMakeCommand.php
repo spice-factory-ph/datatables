2f3f7f962b87073362ebaed9f56578d0dafeaeab
Ian Jay Bronola <ian.bronola@appetiser.com.au>
2023-12-13 09:22:45 +0800

    Refactor ScriptsMakeCommand to create a new view file

--- src/Providers/Console/Commands/ScriptsMakeCommand.php
+++ src/Providers/Console/Commands/ScriptsMakeCommand.php
@@ -4,24 +4,66 @@ declare(strict_types=1);
 
 namespace Ianjaybronola\SpiceDatatable\Providers\Console\Commands;
 
-use Illuminate\Console\GeneratorCommand;
+use Illuminate\View\Factory;
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputArgument;
 
-class ScriptsMakeCommand extends GeneratorCommand
+class ScriptsMakeCommand extends Command
 {
     protected $signature = "spice-make:scripts {name}";
-
-    protected $name = "scripts";
     protected $description = 'Create scripts for datatable';
+
+
+    /**
+     * @var File
+     */
+    private $file;
+
+    /**
+     * @var Factory
+     */
+    private $view;
 
     protected function getStub()
     {
         return __DIR__ . "/../../../../stubs/scripts.stub";
     }
 
-    protected function getDefaultNamespace($rootNamespace): string
+    /**
+     * Create a new command instance.
+     *
+     * @param Filesystem $file
+     * @param Factory    $viewFactory
+     */
+    public function __construct(Filesystem $file, Factory $viewFactory)
     {
-        $scriptName = strtolower($this->argument('name'));
-        return config('view.paths')[0] . '\\' . $scriptName . '\\' . $scriptName . '\\scripts.blade.php';
+        $this->file = $file;
+        $this->view = $viewFactory;
+
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $view = $this->argument('name');
+        $path = config('view.paths')[0] . '/' . $view . '.blade.php';
+        $directory = dirname($path);
+
+        if ($this->file->exists($path)) {
+            $this->error("A view already exists at {$path}!");
+        }
+
+        if (!$this->file->exists($directory)) {
+            $this->file->makeDirectory($directory, 0777, true);
+        }
+
+        $this->file->put($path, $view);
+        $this->info("Created a new view at {$path}");
     }
 }
